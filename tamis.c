46e0c1c610c6c2f4f90b3dec87d10c00d222584f
def@silenus.hom <def@silenus.home>
2007-05-13 23:49:51 +0200

    Working version but: - need to do -1 in timing - keep eip as is in the protected example

--- tamis.c
+++ tamis.c
@@ -1,3 +1,22 @@
+/*
+    tamis.c - tamis core code
+    Copyright (C) 2007  Frederik Deweerdt <frederik.deweerdt@gmail.com>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
 #include <memory.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -6,39 +25,87 @@
 #include <execinfo.h>
 #include <stdint.h>
 #include <sys/mman.h>
+#include <dlfcn.h>
+#include <pthread.h>
+#include <sys/time.h>
 
 #include "tamis.h"
 
+#if 1
+#define pr_debug(x, a...) do { \
+			 	char __buf[4096]; \
+				sprintf(__buf, x, ##a); \
+				fprintf(stderr, "0x%x	%s", (int)pthread_self(), __buf); \
+			  } while(0);
+#else
+#define pr_debug(...) do {} while(0)
+#endif
+
 static __thread struct tamis_tls tamis_private;
+
+static int (*orig_pthread_mutex_lock)(pthread_mutex_t *mutex);
+static int (*orig_pthread_mutex_unlock)(pthread_mutex_t *mutex);
+
+int pthread_mutex_lock(pthread_mutex_t *mutex)
+{
+	tamis_private.lock_level++;
+	return orig_pthread_mutex_lock(mutex);
+}
+int pthread_mutex_unlock(pthread_mutex_t *mutex)
+{
+	tamis_private.lock_level--;
+	return orig_pthread_mutex_unlock(mutex);
+}
 
 static int insn_len(uint8_t *opcode)
 {
 	switch (opcode[0]) {
+		case 0xa3:
+			return 5;
 		case 0xc7:
-			return 6;
+			return 7;
 		case 0x8b:
-			return 1;
+			return 2;
 		default:
+			fprintf(stderr, "unknown opcode 0x%02x\n", opcode[0]);
 			return -1;
 	}
 }
-static void dump_mem(void *mem, size_t len, size_t size)
+static void __attribute__((unused)) dump_mem(void *mem, size_t len, size_t size)
 {
 	uint8_t *u8;
+#if 0
 	uint16_t *u16;
+#endif
 	uint32_t *u32;
 	int i;
+	char buf[4096] = "";
+	char buf2[4096] = "";
 
 	switch (size) {
 		case 1:
 			u8 = mem;
-			for (i=0; i < len / size; i++) {
+			for (i=0; i <= len / size; i++) {
 				if (!(i%8))
-					printf("%02x ", u8[i]);
+					pr_debug("%02x ", u8[i]);
 			}
+			break;
+		case 4:
+			u32 = mem;
+			for (i=0; i <= len / size; i++) {
+				buf2[0] = '\0';
+				sprintf(buf2, "%08x ", u32[i]);
+				strcat(buf, buf2);
+				if (!(i%8)) {
+					pr_debug("%s\n", buf);
+					buf[0] = '\0';
+				}
+			}
+			break;
 		default:
-			printf("Unhandled len %d\n", size);
+			pr_debug("Unhandled size %d\n", size);
 	}
+	pr_debug("\n");
 	return;
 }
 
@@ -66,24 +133,34 @@ static void exepage_unprotect(void *mem)
 	mprotect(p, PAGE_SIZE, PROT_READ|PROT_WRITE);
 }
 
+static pthread_mutex_t tamis_lock = PTHREAD_MUTEX_INITIALIZER;
+
 static void signal_trap(int signum, siginfo_t * info, void* stack)
 {
 	int8_t *eip;
-	ucontext_t *ucontext = (ucontext_t *)stack;
+	ucontext_t *ucontext = stack;
 
 	eip = (int8_t *)ucontext->uc_mcontext.gregs[REG_EIP] - 1;
 
 	exepage_unprotect(eip);
-	//printf("restoring %p\n", eip);
+	pr_debug("restoring %p\n", eip);
 	eip[0] = tamis_private.old_opcode;	
 	exepage_protect(eip);
 
 	ucontext->uc_mcontext.gregs[REG_EIP] = (int)eip;
 	tamis_protect(tamis_private.to_protect_mem,
 		      tamis_private.to_protect_len);
+
+	/* locked in signal_segv */
+	pthread_mutex_unlock(&tamis_lock);
+
 	return;
 }
 
+/* 
+ * TODO: the whole memzone handling sucks,
+ * use something dynamic
+ */
 static struct tamis_memzone _mz[512];
 static int imz = 0;
 
@@ -109,8 +186,10 @@ static inline struct tamis_memzone *find_memzone(void *p)
 		if (mz_includes(p, &_mz[i]))
 			return &_mz[i];
 	}
+	pr_debug("find return null for %p\n", p);
 	return NULL;
 }
+
 static void signal_segv(int signum, siginfo_t * info, void* stack)
 {
 	void **ebp = 0;
@@ -118,23 +197,29 @@ static void signal_segv(int signum, siginfo_t * info, void* stack)
 	void *next_insn = 0;
 	struct tamis_memzone *mz;
 	int len;
-	ucontext_t *ucontext = (ucontext_t *)stack;
+	ucontext_t *ucontext = stack;
 
+	/* unlocked after signal_trap */
+	pthread_mutex_lock(&tamis_lock);
 
+	eip = (void *)ucontext->uc_mcontext.gregs[REG_EIP];
+	pr_debug("sigsegv illegal access %p, eip %p\n", info->si_addr, eip);
 	mz = find_memzone(info->si_addr);
 
+	/*dump_mem(stack, 256, 4);*/
 	if (mz) {
-		eip = (void *)ucontext->uc_mcontext.gregs[REG_EIP];
+		
 		len = insn_len(eip);
 		if (len < 0) {
-			printf("Unknown opcode %02x at %p\n", ((uint8_t*)eip)[0], eip);
+			fprintf(stderr, "Unknown opcode %02x at %p\n", ((uint8_t*)eip)[0], eip);
 			exit(0);
 		}
-		next_insn = eip + len;
+		/* TODO -1 or not ??? */
+		next_insn = eip + len - 1;
 
 		exepage_unprotect(next_insn);
 		tamis_private.old_opcode = ((uint8_t *)next_insn)[0];
-		//printf("setting %p\n", next_insn);
+		pr_debug("setting %p 0x%02x\n", next_insn, ((uint8_t *)next_insn)[0]);
 		((uint8_t *)next_insn)[0] = BREAK_INSN;
 		exepage_protect(next_insn);
 
@@ -144,10 +229,13 @@ static void signal_segv(int signum, siginfo_t * info, void* stack)
 		tamis_unprotect(mz);
 
 		if (mz_includes(info->si_addr, mz)) {
-			/* */
+			/* even number of lockings == NOK */
+			if ((tamis_private.lock_level & 1) ^ 1) {
+				fprintf(stderr, "suspicious access from %p\n", eip);
+			}
 		}
 	} else {
-		printf("not our sigsegv at %p\n", info->si_addr);
+		fprintf(stderr, "not our sigsegv at %p\n", info->si_addr);
 		while(1);
 		exit(0);
 	}
@@ -164,15 +252,20 @@ void tamis_unshare(void *p)
 	del_memzone(mz);
 }
 
-void tamis_share(void *p, size_t len)
+int tamis_share(void *p, size_t len)
 {
 	add_memzone(p, len);
-	tamis_protect(p, len);
+	return tamis_protect(p, len);
 }
 
+#define HIJACK(a, x, y) if (!(orig_##x = dlsym(a , y))) {\
+			   fprintf(stderr, "symbol %s() not found, exiting\n", #y);\
+                	   exit(-1);\
+                        }
 int tamis_init()
 {
 	struct sigaction action;
+	void *lib_handle = NULL;
 
 	memset(&action, 0, sizeof(action));
 	action.sa_sigaction = signal_segv;
@@ -187,48 +280,101 @@ int tamis_init()
 	if(sigaction(SIGTRAP, &action, NULL) < 0) {
 		return -1;
 	}
+
+	if ( (lib_handle = dlopen("libpthread.so", RTLD_NOW)) == NULL) {
+		if ( (lib_handle = dlopen("libpthread.so.0", RTLD_NOW)) == NULL) {
+			fprintf(stderr, "error loading libpthread!\n");
+			return -1;
+		}
+	}
+
+	HIJACK(lib_handle, pthread_mutex_lock, "pthread_mutex_lock");
+	HIJACK(lib_handle, pthread_mutex_unlock, "pthread_mutex_unlock");
+
 	return 0;
 }
 
-#define SIZE (sizeof(int)*126)
-int main()
+
+#define LOOPS 50
+static int my_shared_var;
+static pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
+
+void *f_protected(void *arg)
+{
+	int i;
+	for (i=0; i < LOOPS; i++) {
+		pthread_mutex_lock(&m);
+		my_shared_var = i;
+		pthread_mutex_unlock(&m);
+	}
+	return NULL;
+}
+void *f_unprotected(void *arg)
+{
+	int i;
+	for (i=0; i < LOOPS; i++) {
+		my_shared_var = i;
+	}
+	return NULL;
+}
+
+void thread_test()
+{
+	pthread_t t1;
+	pthread_t t2;
+
+	tamis_share(&my_shared_var, sizeof(my_shared_var));
+	pthread_create(&t1, NULL, f_protected, NULL);
+	pthread_create(&t2, NULL, f_unprotected, NULL);
+	pthread_join(t1, NULL);
+	pthread_join(t2, NULL);
+}
+
+void *timing(void *arg)
 {
 	int *ptr;
 	int *ptr2;
 	int *ptr3;
 	int i, loops=10000;
 	struct timeval tv1, tv2;
-
-	if (tamis_init()) {
-		perror("failed to init tamis");
-		exit(0);
-	}
-
+	
+#define SIZE (sizeof(int)*126)
 	ptr = malloc(SIZE);
 	ptr3 = malloc(4096);
 	ptr2 = malloc(SIZE);
 
 	tamis_share(ptr, SIZE);
 
-	gettimeofday(&tv1);
+	gettimeofday(&tv1, NULL);
 	for (i=0; i < loops; i++) {
-		ptr[i%(SIZE/sizeof(ptr[0]))] = 2;
+		//ptr[i%(SIZE/sizeof(ptr[0]))] = 2;
+		ptr[0] = 2;
 	}
-	gettimeofday(&tv2);
-	printf("len: %d %d\n", tv2.tv_sec - tv1.tv_sec, tv2.tv_usec - tv1.tv_usec);
+	gettimeofday(&tv2, NULL);
+	gettimeofday(&tv1, NULL);
+	printf("with protection:: %lds %ldus\n", tv2.tv_sec - tv1.tv_sec, tv2.tv_usec - tv1.tv_usec);
 
 	tamis_unshare(ptr);
 
-	gettimeofday(&tv1);
+	gettimeofday(&tv1, NULL);
 	for (i=0; i < loops; i++) {
 		ptr2[i%(SIZE/sizeof(ptr[0]))] = 2;
 	}
-	gettimeofday(&tv2);
-	printf("len: %d %d\n", tv2.tv_sec - tv1.tv_sec, tv2.tv_usec - tv1.tv_usec);
+	gettimeofday(&tv2, NULL);
+	printf("without protection: %lds %ldus\n", tv2.tv_sec - tv1.tv_sec, tv2.tv_usec - tv1.tv_usec);
 
 	free(ptr);
 	free(ptr2);
 	free(ptr3);
 	puts("OK");
-	return 1;
+}
+
+int main()
+{
+	pthread_t t;
+	tamis_init();
+	pthread_create(&t, NULL, timing, NULL);
+	pthread_join(t, NULL);
+	//thread_test();
+	return 0;
 }
--- tamis.h
+++ tamis.h
@@ -1,3 +1,22 @@
+/*
+    tamis.h - Header exporting tamis functions
+    Copyright (C) 2007  Frederik Deweerdt <frederik.deweerdt@gmail.com>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
 #ifndef __TAMIS_H__
 #define __TAMIS_H__
 
@@ -5,6 +24,7 @@ struct tamis_tls {
 	uint8_t old_opcode;
 	void *to_protect_mem;
 	size_t to_protect_len;
+	int lock_level;
 };
 
 struct tamis_memzone {
@@ -15,5 +35,34 @@ struct tamis_memzone {
 #define BREAK_INSN 0xcc
 
 #define PAGE_SIZE 4096
+
+/**
+ * @brief Remove a memory zone from being protected by tamis
+ *
+ * @param p The pointer to the memory to unprotect 
+ **/
+void tamis_unshare(void *p);
+
+/**
+ * @brief Protect memory accesses to a give memory zone with tamis
+ *
+ * @param p The memory zone to protect 
+ * @param len The length of the memory zone to protect
+ *
+ * @return 0 in case of success, -1 otherwise. errno is set with the
+ * approriate value
+ **/
+int tamis_share(void *p, size_t len);
+
+/**
+ * @brief Initialize the tamis library
+ * The following actions are taken:
+ * - install SIGSEGV and SIGTRAP signal handlers
+ * - re-route pthread_mutex_{un,}lock calls
+ *
+ * @return 0 in case of success, -1 otherwise. errno is set with the
+ * approriate value
+ **/
+int tamis_init();
 
 #endif /* __TAMIS_H__ */
